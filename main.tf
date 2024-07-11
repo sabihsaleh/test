b7e89c986a4b096f9c6088858055873a81508050
Sabih Saleh <65428286+sabihsaleh@users.noreply.github.com>
2024-07-11 11:26:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,14 @@ resource "azurerm_storage_account" "example" {
     environment = "dev"
   }
 }
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "api-appserviceplan-pro"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
